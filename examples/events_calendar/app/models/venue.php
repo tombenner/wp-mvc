9a5e15c7e8e5172a671c57eb71430e243f0b0b8a
Tom Benner <tombenner@gmail.com>
2011-04-13 22:54:40 -0400

    Fixin' some indentation that used spaces instead of tabs

--- examples/events_calendar/app/models/venue.php
+++ examples/events_calendar/app/models/venue.php
@@ -14,16 +14,16 @@ class Venue extends AppModel {
 	var $admin_searchable_fields = array('name');
 	
 	public function after_save($object) {
-	    $this->update_sort_name($object);
+		$this->update_sort_name($object);
 	}
 	
 	public function update_sort_name($object) {
-	    $sort_name = $object->name;
-    	$article = 'The';
-    	$article_ = $article.' ';
-    	if(strcasecmp(substr($sort_name, 0, strlen($article_)), $article_) == 0){
-    		$sort_name = substr($sort_name, strlen($article_)).', '.$article;
-    	}
+		$sort_name = $object->name;
+		$article = 'The';
+		$article_ = $article.' ';
+		if (strcasecmp(substr($sort_name, 0, strlen($article_)), $article_) == 0) {
+			$sort_name = substr($sort_name, strlen($article_)).', '.$article;
+		}
 		$this->update($object->__id, array('sort_name' => $sort_name));
 	}
 	
