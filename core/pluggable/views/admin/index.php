64c20df538c023fce7712957937baf8b95c7ec0e
Tom Benner <tombenner@gmail.com>
2012-02-25 17:30:46 -0500

    Adding a missing echo of $params['q'] in the admin index view

--- core/pluggable/views/admin/index.php
+++ core/pluggable/views/admin/index.php
@@ -5,7 +5,7 @@
 	<p class="search-box">
 		<label class="screen-reader-text" for="post-search-input">Search:</label>
 		<input type="hidden" name="page" value="<?php echo MvcRouter::admin_page_param($model->name); ?>" />
-		<input type="text" name="q" value="<?php empty($params['q']) ? '' : $params['q']; ?>" />
+		<input type="text" name="q" value="<?php echo empty($params['q']) ? '' : $params['q']; ?>" />
 		<input type="submit" value="Search" class="button" />
 	</p>
 
