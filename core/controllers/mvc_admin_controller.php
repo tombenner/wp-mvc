1754d1a9ac5992440d30b53f3ef76f844663ac6b
Tom Benner <tombenner@gmail.com>
2012-02-28 23:12:08 -0500

    Simplifying MvcAdminController::create_or_save() and adding create() and save()

--- core/controllers/mvc_admin_controller.php
+++ core/controllers/mvc_admin_controller.php
@@ -38,23 +38,41 @@ class MvcAdminController extends MvcController {
 	}
 	
 	public function create_or_save() {
-		if (!empty($this->params['data'])) {
-			if (!empty($this->params['data'][$this->model->name])) {
-				$object = $this->params['data'][$this->model->name];
-				if (empty($object['id'])) {
-					$this->model->create($this->params['data']);
-					$id = $this->model->insert_id;
-					$url = MvcRouter::admin_url(array('controller' => $this->name, 'action' => 'edit', 'id' => $id));
-					$this->flash('notice', 'Successfully created!');
-					$this->redirect($url);
+		if (!empty($this->params['data'][$this->model->name])) {
+			$object = $this->params['data'][$this->model->name];
+			if (empty($object['id'])) {
+				$this->model->create($this->params['data']);
+				$id = $this->model->insert_id;
+				$url = MvcRouter::admin_url(array('controller' => $this->name, 'action' => 'edit', 'id' => $id));
+				$this->flash('notice', 'Successfully created!');
+				$this->redirect($url);
+			} else {
+				if ($this->model->save($this->params['data'])) {
+					$this->flash('notice', 'Successfully saved!');
+					$this->refresh();
 				} else {
-					if ($this->model->save($this->params['data'])) {
-						$this->flash('notice', 'Successfully saved!');
-						$this->refresh();
-					} else {
-						$this->flash('error', $this->model->validation_error_html);
-					}
+					$this->flash('error', $this->model->validation_error_html);
 				}
+			}
+		}
+	}
+	
+	public function create() {
+		if (!empty($this->params['data'][$this->model->name])) {
+			$id = $this->model->create($this->params['data']);
+			$url = MvcRouter::admin_url(array('controller' => $this->name, 'action' => 'edit', 'id' => $id));
+			$this->flash('notice', 'Successfully created!');
+			$this->redirect($url);
+		}
+	}
+	
+	public function save() {
+		if (!empty($this->params['data'][$this->model->name])) {
+			if ($this->model->save($this->params['data'])) {
+				$this->flash('notice', 'Successfully saved!');
+				$this->refresh();
+			} else {
+				$this->flash('error', $this->model->validation_error_html);
 			}
 		}
 	}
