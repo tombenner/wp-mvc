5b0639411bdfa926762021682dcb5ede21c0fb76
beezee <brian.zeligson@gmail.com>
2012-03-15 13:01:34 -0400

    set server vars to prevent wp from trying to redirect during cli calls resulting in  silent failure. see http://vocecommunications.com/blog/2011/03/running-wordpress-from-command-line/

--- core/wpmvc.php
+++ core/wpmvc.php
@@ -3,6 +3,13 @@
 $wordpress_path = getenv('WPMVC_WORDPRESS_PATH');
 $wordpress_path = $wordpress_path ? rtrim($wordpress_path, '/').'/' : dirname(__FILE__).'/../../../../';
 
+$_SERVER = array(
+    "HTTP_HOST" => "localhost",
+    "SERVER_NAME" => "localhost",
+    "REQUEST_URI" => "/",
+    "REQUEST_METHOD" => "GET"
+);
+
 require_once $wordpress_path.'wp-load.php';
 
 $shell = new MvcShellDispatcher($argv);
