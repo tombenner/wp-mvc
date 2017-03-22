b9e03dfc9daa49355af3050e1392163be7e4b852
Mikolaj Romanski <mikolaj@doromi.net>
2017-03-22 06:05:27 +0100

    fix php 7 error for reassign

--- core/controllers/mvc_controller.php
+++ core/controllers/mvc_controller.php
@@ -252,7 +252,7 @@ class MvcController {
     }
     
     protected function include_view($path, $view_vars=array()) {
-        extract($view_vars);
+		extract($view_vars, EXTR_IF_EXISTS);
         $path = preg_replace('/^admin_([^\/]+)/', 'admin/$1', $path);
         $filepath = $this->file_includer->find_first_app_file_or_core_file('views/'.$path.'.php');
         if (!$filepath) {
