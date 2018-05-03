06c9ca647fe7a0a2d3d95da9714b8df3b41e3170
Felipe Elia <felipe.elia@gmail.com>
2018-05-03 16:05:08 -0300

    Better style for forms in WP Dashboard

--- core/templates/plugin/app/views/admin/add.php
+++ core/templates/plugin/app/views/admin/add.php
@@ -1,7 +1,9 @@
 <h2>Add <?php echo $name_titleized; ?></h2>
 <?php
 echo '
-<?php echo $this->form->create($model->name); ?>
+<?php echo $this->form->create($model->name, array(\'is_admin\' => $this->is_admin)); ?>
+<?php echo $this->form->open_admin_table(); ?>
 <?php echo $this->form->input(\'name\'); ?>
+<?php echo $this->form->close_admin_table(); ?>
 <?php echo $this->form->end(\'Add\'); ?>';
 ?>
