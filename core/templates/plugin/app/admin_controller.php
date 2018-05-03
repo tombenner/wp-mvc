3099c72a9bea5f73cc8683de651e55b0492f151d
Felipe Elia <felipe.elia@gmail.com>
2018-05-03 16:26:24 -0300

    Remove close PHP tags from pure PHP files

--- core/templates/plugin/app/admin_controller.php
+++ core/templates/plugin/app/admin_controller.php
@@ -1,9 +1,7 @@
 <?php echo "<?php\n"; ?>
 
 class Admin<?php echo $name_pluralized ?>Controller extends MvcAdminController {
-    
-    var $default_columns = array('id', 'name');
-    
-}
 
-<?php echo '?>'; ?>
+    var $default_columns = array('id', 'name');
+
+}
